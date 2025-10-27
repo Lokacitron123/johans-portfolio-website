c11e6bffedd07885799fdd536ddcb4a8066cdc22
Johan Lindell <johan.lindell91@gmail.com>
2025-10-27 14:20:42 +0100

    Changed Button CSS, removed hover underline

--- src/app/components/buttons/Button.tsx
+++ src/app/components/buttons/Button.tsx
@@ -19,7 +19,7 @@ export const Button = ({
     <button
       type={type}
       disabled={disabled || loading}
-      className={`group relative flex items-center justify-center gap-2 h-12 px-5 rounded-full text-white bg-gray-900 outline-none transition-all hover:scale-[1.05] active:scale-95 ${className}`}
+      className={`group relative flex items-center justify-center gap-2 h-12 px-5 rounded-full text-white bg-gray-900 dark:bg-white dark:text-gray-900 outline-none transition-all hover:scale-[1.05] active:scale-95 ${className}`}
     >
       {/* Loading state or children */}
       {loading ? (
@@ -30,9 +30,6 @@ export const Button = ({
       ) : (
         <>{children}</>
       )}
-
-      {/* Optional hover effect */}
-      <span className="absolute w-0 group-hover:w-full transition-all ease-out duration-300 h-1 bg-red-600/80 left-0 bottom-0" />
     </button>
   );
 };
